4046ce390d735cc679e5177dfc65ff1e45a15c11
HMuraja <hilla.muraja@hotmail.com>
2023-06-19 00:59:40 +0000

    Add account registration functionalities and templates

--- dbts_project/settings.py
+++ dbts_project/settings.py
@@ -56,7 +56,8 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 ACCOUNT_EMAIL_VERIFICATION = 'none'
-LOGIN_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.RecipeSelection.as_view(), name="home"),
+    path('', views.RecipeSelection.as_view(), name='home'),
 ]
