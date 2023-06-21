d843b2a5a3023fa7eef85f86de8b7a6898a13c80
HMuraja <hilla.muraja@hotmail.com>
2023-06-21 15:38:59 +0000

    Add functional commentinh feature for recipes

--- recipeapp/admin.py
+++ recipeapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -7,3 +7,10 @@ from django_summernote.admin import SummernoteModelAdmin
 class RecipeAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     summernote_fields = ('description', 'ingredients', 'method')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'recipe', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('name', 'email', 'body')
--- recipeapp/views.py
+++ recipeapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
 from django.views import generic, View
 from .models import Recipe
 from .forms import CommentForm
@@ -27,9 +28,40 @@ class RecipeDetails(View):
             {
                 "recipe": recipe,
                 "comments": comments,
+                "commented": False,
                 "liked": liked,
                 "comment_form": CommentForm()
             },
         )
-    
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by("-created_on")
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.save()
+            return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "recipe.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
 
