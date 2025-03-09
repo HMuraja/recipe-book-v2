8fd6f22c16f19191f74b145a43437aa6fd86031d
HMuraja <hilla.muraja@hotmail.com>
2025-03-09 21:58:57 +0000

    Switched to a new database, updated nav menu

--- dbts_project/settings.py
+++ dbts_project/settings.py
@@ -28,11 +28,10 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-hmuraja-p4recipebook-ec2mmbteq1v.ws-eu100.gitpod.io',
-    'localhost', 'dbts-recipe-app-f9995cb6b9df.herokuapp.com']
+    '127.0.0.1', 'dbts-recipe-app-f9995cb6b9df.herokuapp.com']
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -127,7 +126,7 @@ WSGI_APPLICATION = 'dbts_project.wsgi.application'
 #     }
 # }
 DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600)
  }
 
 
