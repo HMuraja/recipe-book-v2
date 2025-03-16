a07d585657783e34d3356f6276583d243ba1806d
HMuraja <hilla.muraja@hotmail.com>
2025-03-16 22:49:50 +0000

    Adjust recipe index and details layout and style

--- dbts_project/settings.py
+++ dbts_project/settings.py
@@ -28,7 +28,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 'dbts-recipe-app-f9995cb6b9df.herokuapp.com']
